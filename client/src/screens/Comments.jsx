0ee98915c07085701f20406cee7aed6467d9992c
eeds90 <eddie.diaz90@icloud.com>
2020-12-03 16:11:48 -0600

    full CRUD working

--- client/src/screens/Comments.jsx
+++ client/src/screens/Comments.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import {  getAllComments } from "../services/comments.js";
+
 
 export default function Comments(props) {
+  
+  const [comments, setComments] = useState([]);
+
+useEffect(() => {
+  const fetchComments = async () => {
+    const commentData = await getAllComments();
+    setComments(commentData);
+  };
+  fetchComments();
+}, []);
   return (
     <div>
       <h3>Comments</h3>
-      {
-        props.comments.map(comment => (
-          <React.Fragment key={comment.id}>
-            <Link to={`/comments/${comment.id}`}><p>{comment.text}</p></Link>
-            {
-              comment.user_id === props.currentUser?.id &&
-              <>
-                <Link to={`/comments/${comment.id}/edit`}><button>Edit</button></Link>
-                <button onClick={() => props.handleDelete(comment.id)}>Delete</button>
-              </>
-            }
-          </React.Fragment>
-        ))
-      }
+      {props.comments.map((comment) => (
+        <React.Fragment key={comment.id}>
+          <Link to={`/comments/${comment.id}`}>
+            <p>{comment.text}</p>
+          </Link>
+          {comment.user_id === props.currentUser?.id && (
+            <>
+              <Link to={`/comments/${comment.id}/edit`}>
+                <button>Edit</button>
+              </Link>
+              <button onClick={() => props.handleDelete(comment.id)}>
+                Delete
+              </button>
+              {comments.map((comment) => (
+                <p>{comment.comment}</p>
+              ))}
+            </>
+          )}
+        </React.Fragment>
+      ))}
       <br />
-      <Link to='/comments/new'><button>Create</button></Link>
+      <Link to="/comments/new">
+        <button>Create</button>
+      </Link>
     </div>
-  )
+  );
 }
