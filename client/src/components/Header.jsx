23e71486469471fda125be3bdf84638c1d85407d
eeds90 <eddie.diaz90@icloud.com>
2020-12-02 19:00:49 -0600

    CSS for Header started

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
 
 export default function Header(props) {
   const { currentUser, handleLogout } = props;
   return (
     <div className="header">
-      
-        <h1>Personal Development</h1>
-        
-      {currentUser ? (
-        <>
-          <p>{currentUser.username}</p>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
-      <hr />
-      {currentUser && (
-        <>
-          <Link to="/comments">comments</Link>
-          <Link to="/questions">questions</Link>
-        </>
-      )}
+      <ul className= 'header_item'>
+        <li className= 'title'>Personal Development</li>
+
+        {currentUser ? (
+          <>
+            <li className= 'current__logout'>
+              <p>{currentUser.username}</p>
+              <button className='magic2' onClick={handleLogout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <div className= 'before_login'>
+            <li className= 'current__user'>
+              <button id= 'magic1'><Link to="/login">Login</Link></button>
+            </li>
+            <li className= 'current__register'>
+             <button id= 'magic1'><Link to="/register">Register</Link></button>
+            </li>
+          </div>
+        )}
+        {/* <hr /> */}
+        {currentUser && (
+          <>
+            <li className= 'comment'>
+              <button id='magic1'><Link to="/comments">comments</Link></button>
+            </li>
+            <li  className= 'question'>
+              <button id='magic1'><Link to="/questions">questions</Link></button>
+            </li>
+          </>
+        )}
+      </ul>
     </div>
   );
 }
--- client/src/screens/Questions.jsx
+++ client/src/screens/Questions.jsx
@@ -1,59 +1,77 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-export default function Questions(props) {
-  const questions = [
-    {
-      questionsText: "What is Personal Development?",
-      answerOptions: [
-        { answerText: "The process of improving oneself", isCorrect: false },
-        { answerText: "A lifelong process", isCorrect: false },
-        {
-          answerText:
-            "The puirsuit of personal growth by gaining knowledge and improving personal skills",
-          isCorrect: false,
-        },
-        { answerText: "All of the above", isCorrect: false },
-      ],
-    },
+// export default function Questions(props) {
+//   const questions = [
+//     {
+//       questionsText: "What is Personal Development?",
+//       answerOptions: [
+//         { answerText: "The process of improving oneself", isCorrect: false },
+//         { answerText: "A lifelong process", isCorrect: false },
+//         {
+//           answerText:
+//             "The puirsuit of personal growth by gaining knowledge and improving personal skills",
+//           isCorrect: false,
+//         },
+//         { answerText: "All of the above", isCorrect: false },
+//       ],
+//     },
 
-    {
-      questionsText: "Things that you must have in order to live/survive?",
-      answerOptions: [
-        { answerText: "Goal", isCorrect: false },
-        { answerText: "Perdonality", isCorrect: false },
-        { answerText: "Needs", isCorrect: false },
-        { answerText: "All of the above", isCorrect: false },
-      ],
-    },
+//     {
+//       questionsText: "Things that you must have in order to live/survive?",
+//       answerOptions: [
+//         { answerText: "Goal", isCorrect: false },
+//         { answerText: "Perdonality", isCorrect: false },
+//         { answerText: "Needs", isCorrect: false },
+//         { answerText: "All of the above", isCorrect: false },
+//       ],
+//     },
 
-    {
-      questionsText: "What is confidence?",
-      answerOptions: [
-        { answerText: "Being cocky", isCorrect: false },
-        { answerText: "Wanting thing more than others", isCorrect: false },
-        { answerText: "A feeling of self-assurance arising from one's appreciation of one's abilities or qualities", isCorrect: false },
-        { answerText: "None of the above", isCorrect: false },
-      ],
-    },
+//     {
+//       questionsText: "What is confidence?",
+//       answerOptions: [
+//         { answerText: "Being cocky", isCorrect: false },
+//         { answerText: "Wanting thing more than others", isCorrect: false },
+//         { answerText: "A feeling of self-assurance arising from one's appreciation of one's abilities or qualities", isCorrect: false },
+//         { answerText: "None of the above", isCorrect: false },
+//       ],
+//     },
 
-    {
-      questionsText: "A skill that has been developed?",
-      answerOptions: [
-        { answerText: "Carrer", isCorrect: false },
-        { answerText: "Job", isCorrect: false },
-        { answerText: "Ability", isCorrect: false },
-        { answerText: "Learning style", isCorrect: false },
-      ],
-    },
+//     {
+//       questionsText: "A skill that has been developed?",
+//       answerOptions: [
+//         { answerText: "Carrer", isCorrect: false },
+//         { answerText: "Job", isCorrect: false },
+//         { answerText: "Ability", isCorrect: false },
+//         { answerText: "Learning style", isCorrect: false },
+//       ],
+//     },
 
-    {
-      questionsText: "Things that you must have in order to live/survive?",
-      answerOptions: [
-        { answerText: "Goal", isCorrect: false },
-        { answerText: "Perdonality", isCorrect: false },
-        { answerText: "Needs", isCorrect: false },
-        { answerText: "All of the above", isCorrect: false },
-      ],
-    },
-  ];
-}
+//     {
+//       questionsText: "Things that you must have in order to live/survive?",
+//       answerOptions: [
+//         { answerText: "Goal", isCorrect: false },
+//         { answerText: "Perdonality", isCorrect: false },
+//         { answerText: "Needs", isCorrect: false },
+//         { answerText: "All of the above", isCorrect: false },
+//       ],
+//     },
+//   ];
+
+//   return (
+//     <div className='question'>
+//       {false ? (
+
+//       )}
+//         </div> 
+//   )
+
+
+
+
+
+
+
+
+
+
+// }
