f5353c9ced395849cc399ab41b49b9b3e39a9a8d
eeds90 <eddie.diaz90@icloud.com>
2020-12-04 14:42:20 -0600

    deploy

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://heroku.com/apps/personaldevelopment-api' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://personaldevelopment-api.herokuapp.com' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl
