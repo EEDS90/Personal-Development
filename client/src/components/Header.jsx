3993bb0f6bbfae314569f02ed64caec5aa28634e
eeds90 <eddie.diaz90@icloud.com>
2020-12-04 00:45:19 -0600

    additions to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,34 +6,57 @@ export default function Header(props) {
   const { currentUser, handleLogout } = props;
   return (
     <div className="header">
-      <ul className= 'header_item'>
-        <li className= 'title'>My Personal Legend</li>
+      <ul className="header_item">
+        <li className="title">My Personal Legend</li>
 
         {currentUser ? (
           <>
-            <li className= 'current__logout'>
+            <li className="current_logout">
               <p>{currentUser.username}</p>
-              <button className='magic2' onClick={handleLogout}>Logout</button>
+              <button className="magic2" onClick={handleLogout}>
+                Logout
+              </button>
             </li>
+
+            <li className="home">
+              <button id="magic1">
+                <Link to="/">Home</Link>
+              </button>
+            </li>
+
+            <li className="about">
+              <button id="magic1">
+                <Link to="/">About</Link>
+              </button>
+            </li>
+
           </>
         ) : (
-          <div className= 'before_login'>
-            <li className= 'current__user'>
-              <button id= 'magic1'><Link to="/login">Login</Link></button>
+          <div className="before_login">
+            <li className="current_user">
+              <button id="magic1">
+                <Link to="/login">Login</Link>
+              </button>
             </li>
-            <li className= 'current__register'>
-             <button id= 'magic1'><Link to="/register">Register</Link></button>
+            <li className="current_register">
+              <button id="magic1">
+                <Link to="/register">Register</Link>
+              </button>
             </li>
           </div>
         )}
         {/* <hr /> */}
         {currentUser && (
           <>
-            <li className= 'comment'>
-              <button id='magic1'><Link to="/comments">comments</Link></button>
+            <li className="comment">
+              <button id="magic1">
+                <Link to="/comments">comments</Link>
+              </button>
             </li>
-            <li  className= 'question'>
-              <button id='magic1'><Link to="/questions">questions</Link></button>
+            <li className="question">
+              <button id="magic1">
+                <Link to="/questions">questions</Link>
+              </button>
             </li>
           </>
         )}
