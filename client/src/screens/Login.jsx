56625572acfe631cfed81aad1012ad39a1858c4a
eeds90 <eddie.diaz90@icloud.com>
2020-12-04 01:09:41 -0600

    login css 90% completed

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,46 +1,58 @@
-import { useState } from 'react';
-
+import { useState } from "react";
+import "./Login.css";
 
 export default function Login(props) {
   const [formData, setFormData] = useState({
     username: "",
-    password: ""
-  })
+    password: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      props.handleLogin(formData);
-    }}>
-      <h3>Login</h3>
-      <label>Username:
-        <input
-          type='text'
-          name='username'
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>Password:
-        <input
-          type='password'
-          name='password'
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-    
-      <button>Submit</button>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.handleLogin(formData);
+      }}
+    >
+      <div className="loginBox">
+        <h3>Login</h3>
+        <div className="username">
+          <label>
+            
+            <input className="username_one"
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <br />
+         
+          <label>
+            
+            <input className="password_one"
+              type="password"
+            name="password"
+            placeholder="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+        
+        <br />
+      
+        <button className="submit_button">Submit</button>
+        </div>
     </form>
-  )
+  );
 }
