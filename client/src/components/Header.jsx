50158111beca0fa2aebc1b3076eb23ab0bdcffee
eeds90 <eddie.diaz90@icloud.com>
2020-12-06 16:13:53 -0600

    header css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,59 +9,61 @@ export default function Header(props) {
       <div className="header">
         <div className="title">iAlchemy</div>
         <hr />
-        <ul className="header-item">
-          {currentUser ? (
-            <>
-              <li className="home">
-                <Link to="/home">
-                  {" "}
-                  <a> Home</a>
-                </Link>
-              </li>
+        <div className="header5">
+          <ul className="header-item">
+            {currentUser ? (
+              <>
+                <li className="home">
+                  <Link to="/home">
+                    {" "}
+                    <a> Home</a>
+                  </Link>
+                </li>
 
-              <li className="about">
-                <Link to="/aboutus">
-                  <a>About Us</a>
-                </Link>
-              </li>
-            </>
-          ) : (
-            <div className="before_login">
-              <li className="current_user">
-                <Link to="/login">
-                  <a>Sign In</a>
-                </Link>
-              </li>
-              <li className="current_register">
-                <Link to="/register">
-                  <a>Sign Up</a>
-                </Link>
-              </li>
-            </div>
-          )}
+                <li className="about">
+                  <Link to="/aboutus">
+                    <a>About Us</a>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <div className="before_login">
+                <li className="current_user">
+                  <Link to="/login">
+                    <a>Sign In</a>
+                  </Link>
+                </li>
+                <li className="current_register">
+                  <Link to="/register">
+                    <a>Sign Up</a>
+                  </Link>
+                </li>
+              </div>
+            )}
 
-          {currentUser && (
-            <>
-              <li className="comment">
-                <Link to="/comments">
-                  <a>Comments</a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/questions">
-                  <a>Take Quiz</a>
-                </Link>
-              </li>
+            {currentUser && (
+              <>
+                <li className="comment">
+                  <Link to="/comments">
+                    <a>Comments</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/questions">
+                    <a>Take Quiz</a>
+                  </Link>
+                </li>
 
-              <li className="current_logout">
-                <p>{currentUser.username}</p>
-                <button className="magic2" onClick={handleLogout}>
-                  Sign Out
-                </button>
-              </li>
-            </>
-          )}
-        </ul>
+                <li className="current_logout">
+                  <p>{currentUser.username}</p>
+                  <button className="magic2" onClick={handleLogout}>
+                    Sign Out
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
