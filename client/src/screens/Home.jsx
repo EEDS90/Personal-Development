e132b13018d1ce861e2cd937d86005b80d12ba2d
eeds90 <eddie.diaz90@icloud.com>
2020-12-06 15:03:48 -0600

    home css completed

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -10,6 +10,13 @@ export default function Home() {
 
         <div>
           <img id="image1" src="/assets2/Journey.jpeg" />
+          <p className="quote">
+            “I wish for you a life of wealth, health and happiness; a life in
+            which you give to yourself the gift of patience, the virtue of
+            reason, the value of knowledge, and the influence of faith in your
+            own ability to dream about and to achieve worthy rewards.” – Jim
+            Rohn
+          </p>
         </div>
       </div>
     </div>
